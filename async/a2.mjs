3f3052c2801f05f3c4d95307f561caa95117aa2d
Gero <geronimoooooooo@gmail.com>
2024-04-05 16:32:43 +0200

    asdf

--- async/a2.mjs
+++ async/a2.mjs
@@ -10,3 +10,22 @@ console.log(bro);
 
 let prom = await Promise.resolve("ak").then(result =>{return result;})
 console.log(prom);
+
+let jsondata;    
+fetch("https://jsonplaceholder.typicode.com/posts/1").then(
+        function(u){ return u.json();}
+      ).then(
+        function(json){
+          jsondata = json;
+        }
+      )
+
+      let json="";
+
+      const getJson = async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+        return await response.json()       
+      }
+      
+      json = await getJson()
+      console.log(json.title)//prints: sunt aut facere repellat provident
